94d3cb5ce99cc1b293316a3afbd17cd06269464b
Sergey Adeninskiy <adeninskiy@akura.ru>
2015-10-01 12:53:08 +0300

    ResolveProperties don't returns null properties

--- src/Dapper.FluentMap.Dommel/Resolvers/DommelPropertyResolver.cs
+++ src/Dapper.FluentMap.Dommel/Resolvers/DommelPropertyResolver.cs
@@ -24,7 +24,7 @@ namespace Dapper.FluentMap.Dommel.Resolvers
                 foreach (var property in FilterComplexTypes(type.GetProperties()))
                 {
                     var propertyMap = entityMap.PropertyMaps.FirstOrDefault(p => p.PropertyInfo.Name == property.Name);
-                    if (propertyMap == null || !propertyMap.Ignored)
+                    if (propertyMap != null && !propertyMap.Ignored)
                     {
                         yield return property;
                     }
